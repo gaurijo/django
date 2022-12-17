6251ddc95607fff473313669018ef9aa91177959
Gauri Joshi <gauri728@gmail.com>
2022-12-16 23:10:42 -0800

    views and templates + routes

--- urls.py
+++ urls.py
@@ -2,6 +2,12 @@ from django.urls import path
 
 from . import views #from root directory import views 
 
+# like ruby routes.rb file 
+
 urlpatterns = [
-  path("", views.index, name="index")
+  path("", views.index, name="index"), 
+  path("<str:name>", views.greet, name="greet"),
+  # path("gauri", views.gauri, name="gauri")
 ]
+
+# different urls for different views 
--- views.py
+++ views.py
@@ -1,6 +1,15 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+# Create your view logic here
 def index(request):
-  return HttpResponse("Hello, world!")
+  return render(request, "HELLO/index.html")
+
+# def gauri(request):
+#   return HttpResponse("Hello, Gauri")
+
+# now this makes the url path dynamic 
+def greet(request, name): 
+  return render(request, "HELLO/greet.html", {
+    "name": name.capitalize()
+  })
